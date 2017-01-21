97fa549747e6a24444987af674b943ada3e4599a
nekonez <sweraba263@gmail.com>
2017-01-21 05:08:32 +0000

    Basic bullshit.

---
+++ index.js
@@ -0,0 +1,11 @@
+const Client = require("./lib/Client");
+const Request = require("./lib/handlers/Requester");
+
+function Discord(token, options) {
+    return new Client(token, options)
+}
+
+Discord.Client = Client;
+Discord.Request = Request;
+
+module.exports = Discord;
