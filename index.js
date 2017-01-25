ce70c2e56abd7fc9546d4a0d257ef4d89bae0e22
nekonez <sweraba263@gmail.com>
2017-01-25 01:43:41 +0000

    channel.id

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 "use strict";
 const Client = require("./lib/Client");
-const Request = require("./lib/handlers/Requester");
 
 function Discord(token, options) {
     return new Client(token, options)
 }
 
 Discord.Client = Client;
-Discord.Request = Request;
 
 module.exports = Discord;
