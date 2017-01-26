f538ac4a2e47f970076b5e6b6037473973b5b608
nekonez <sweraba263@gmail.com>
2017-01-26 06:12:41 +0000

    awau

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 "use strict";
 const Client = require("./lib/Client");
+const WebhookClient = require("./lib/webhook/WebhookClient")
 
 function Discord(token, options) {
     return new Client(token, options)
 }
 
 Discord.Client = Client;
+Discord.Webhook = WebhookClient;
 
 module.exports = Discord;
