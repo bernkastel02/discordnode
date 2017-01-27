a4664f9981cd54653832d0e129c0da4017f4ddc3
nekonez <sweraba263@gmail.com>
2017-01-27 06:26:26 +0000

    rrr

--- examples/ping.js
+++ examples/ping.js
@@ -3,10 +3,11 @@ const bot = new DNode("Bot Token")
 // Replace Bot Token with your token
 
 bot.on("ready", () => {
-    console.log("RDY 4 ADVENSHUR");
+    console.log("RDY 4 ADVENSHUR"); // change this message to anything
 })
 
 bot.on("messageSent", (msg) => {
+    // this is a basic command
     if (msg.content === "!ping") {
         bot.sendMessage(msg.channel.id, "Pong!")
     }
